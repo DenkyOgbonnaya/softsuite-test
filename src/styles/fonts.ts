f1466bb5603ed31746459e87dfcbb8e7d07d8e25
DenkyOgbonnaya <denkyogb@gmail.com>
2023-11-14 15:11:47 +0100

    style:syle guide configuration

---
+++ src/styles/fonts.ts
@@ -0,0 +1,35 @@
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const gilroy = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Gilroy-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/Gilroy-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Gilroy-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/Gilroy-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "../../public/fonts/Gilroy-Bold.ttf",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+  variable: "--font-gilroy",
+});
