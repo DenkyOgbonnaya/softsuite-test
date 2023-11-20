e2ab9a3b31d11940ff4695c287a01657c4d4abbb
DenkyOgbonnaya <denkyogb@gmail.com>
2023-11-20 03:30:45 +0100

    refactor:add link on home page to element page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,17 @@
-'use client'
-import Dropdown from "@/components/dropDown";
+"use client";
 import styles from "./page.module.scss";
-import {
-  Button,
-  DateInput,
-  Input,
-  RadioButton,
-  SearchBar,
-  SelectInput,
-  Switch,
-} from "@/components";
+import { Button } from "@/components";
+import { ELEMENTS_ROUTE } from "@/contstants/routes";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Button size="medium" intent="primary">
-        Hello World
-      </Button>
-      <Button isLoading size="medium" intent="primary">
-        Hello World
-      </Button>
-      <Input
-        name="FirstName"
-        type="date"
-        placeholder="First Name"
-        label="Enter first name"
-        error={true}
-        errorMessage="Hello world"
-      />
-      <DateInput
-        name="date"
-        placeholder="State date"
-        label="Enter first name"
-        error={true}
-        errorMessage="Hello world"
-      />
-      <SelectInput name="selec" label="Select one" error>
-        <option value="hell">Hello man</option>
-        <option value="why">Why man</option>
-      </SelectInput>
-
-      <div>
-        <RadioButton label="Hello" name="hello" value="yes" />
-        <RadioButton label="World" name="hello" value="No" />
-      </div>
-
-      <Switch name="hello" label="Active" />
-      <SearchBar placeholder="Search something..." />
-
-      <Dropdown>
-      <p>Hello</p>
-       <p>World</p>
-        <p>Yes</p>
-      </Dropdown>
+      <Link href={ELEMENTS_ROUTE}>
+        <Button size="medium" intent="primary">
+          Goto Dashboard
+        </Button>
+      </Link>
     </main>
   );
 }
